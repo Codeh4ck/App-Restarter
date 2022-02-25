9defb509949e3b42b5cf24c0f1d8c696ad4f2b79
Nikolas Andreou <11696956+Codeh4ck@users.noreply.github.com>
2022-02-25 10:33:45 +0200

    Added global exception form for unhandled exceptions

---
+++ AppRestarter/Forms/FrmException.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AppRestarter.Forms
+{
+    partial class FrmException
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmException));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtExceptionLog = new System.Windows.Forms.TextBox();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btnShutdown = new System.Windows.Forms.Button();
+            this.btnIgnore = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "An exception has been thrown!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(87, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(555, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "App Restarter has ran into an unrecoverable error! Details are displayed below.";
+            // 
+            // txtExceptionLog
+            // 
+            this.txtExceptionLog.BackColor = System.Drawing.SystemColors.Window;
+            this.txtExceptionLog.Location = new System.Drawing.Point(13, 84);
+            this.txtExceptionLog.Multiline = true;
+            this.txtExceptionLog.Name = "txtExceptionLog";
+            this.txtExceptionLog.ReadOnly = true;
+            this.txtExceptionLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtExceptionLog.Size = new System.Drawing.Size(639, 224);
+            this.txtExceptionLog.TabIndex = 4;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnRestart.Location = new System.Drawing.Point(470, 316);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(88, 30);
+            this.btnRestart.TabIndex = 7;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // btnShutdown
+            // 
+            this.btnShutdown.Location = new System.Drawing.Point(564, 316);
+            this.btnShutdown.Name = "btnShutdown";
+            this.btnShutdown.Size = new System.Drawing.Size(88, 30);
+            this.btnShutdown.TabIndex = 6;
+            this.btnShutdown.Text = "Shutdown";
+            this.btnShutdown.UseVisualStyleBackColor = true;
+            this.btnShutdown.Click += new System.EventHandler(this.btnShutdown_Click);
+            // 
+            // btnIgnore
+            // 
+            this.btnIgnore.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnIgnore.Location = new System.Drawing.Point(376, 316);
+            this.btnIgnore.Name = "btnIgnore";
+            this.btnIgnore.Size = new System.Drawing.Size(88, 30);
+            this.btnIgnore.TabIndex = 8;
+            this.btnIgnore.Text = "Ignore";
+            this.btnIgnore.UseVisualStyleBackColor = true;
+            this.btnIgnore.Click += new System.EventHandler(this.btnIgnore_Click);
+            // 
+            // FrmException
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 354);
+            this.Controls.Add(this.btnIgnore);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnShutdown);
+            this.Controls.Add(this.txtExceptionLog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmException";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "App Restarter - Exception";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtExceptionLog;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button btnShutdown;
+        private System.Windows.Forms.Button btnIgnore;
+    }
+}
