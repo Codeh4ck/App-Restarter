713a5282510b5c8f0b45fb26ec63963aaa8d17f5
Nikolas Andreou <11696956+Codeh4ck@users.noreply.github.com>
2022-02-25 10:41:14 +0200

    Added Update model

---
+++ AppRestarter/Models/Update.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppRestarter.Models
+{
+    public class Update
+    {
+        public Version Version { get; set; }
+        public string ChangeLog { get; set; }
+        public string DownloadUrl { get; set; }
+        public DateTime PublishedAt { get; set; }
+    }
+}
