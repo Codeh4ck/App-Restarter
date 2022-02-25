c5c3faedfd54b49fa256c31cb4d67c9a72d210e3
Nikolas Andreou <11696956+Codeh4ck@users.noreply.github.com>
2022-02-25 10:30:32 +0200

    Added updater program that replaces affected files or rolls back on error

---
+++ AppRestarter.Updater/ExitCodes.cs
@@ -0,0 +1,12 @@
+﻿namespace AppRestarter.Updater
+{
+    public enum ExitCodes
+    {
+        CouldNotValidateUpdateFiles = 1000,
+        CouldNotKillAppRestarter = 1001,
+        CouldNotBackupAppRestarterFiles = 1002,
+        CouldNotInstallNewAppRestarterFiles = 1003,
+        CouldNotCleanupUpdateFiles = 1004,
+        CouldNotRollbackUpdate = 1005
+    }
+}
