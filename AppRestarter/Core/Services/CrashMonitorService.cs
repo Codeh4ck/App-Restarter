3be913aa1c5a5f74ba6ce127faedaf127ef42eaa
Nikolas Andreou <11696956+Codeh4ck@users.noreply.github.com>
2022-02-25 10:31:42 +0200

    Added crash monitor service that checks if a program crashed due to misconfiguration

---
+++ AppRestarter/Core/Services/CrashMonitorService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using AppRestarter.Models;
+
+namespace AppRestarter.Core.Services
+{
+    public class CrashMonitorService : SingletonBase<ICrashMonitorService, CrashMonitorService>, ICrashMonitorService
+    {
+        public event ICrashMonitorService.SignalMonitorTerminationEvent OnSignalMonitorTermination;
+
+        private readonly ILogService _logService;
+        private readonly ConcurrentDictionary<Guid, CrashMonitorEntry> _crashMonitorEntries;
+
+        public CrashMonitorService()
+        {
+            _logService = LogService.GetInstance();
+            _crashMonitorEntries = new();
+        }
+
+        public void RegisterCrashedApp(WatchedApp app)
+        {
+            if (_crashMonitorEntries.ContainsKey(app.Id))
+            {
+                CrashMonitorEntry existingEntry = _crashMonitorEntries[app.Id];
+                existingEntry.HitCount++;
+
+                if (existingEntry.HitCount >= existingEntry.App.CrashThreshold)
+                {
+                    _logService.AddLog(app, "Hit application crash threshold. Killing application and stopping monitoring.");
+
+                    SignalMonitorTermination(app);
+                    UnregisterCrashedApp(app);
+                }
+
+                return;
+            }
+
+            CrashMonitorEntry entry = new()
+            {
+                App = app,
+                HitCount = 0
+            };
+
+            if (_crashMonitorEntries.TryAdd(app.Id, entry))
+                _logService.AddLog(app, "Detected unexpected shutdown (misconfiguration?). Began monitoring app for crashes...");
+
+        }
+
+        public void UnregisterCrashedApp(WatchedApp app)
+        {
+            if(_crashMonitorEntries.TryRemove(app.Id, out _))
+                _logService.AddLog(app, "Removed application from crash monitor.");
+        }
+
+        public void SignalMonitorTermination(WatchedApp app) => OnSignalMonitorTermination?.Invoke(app);
+    }
+}
---
+++ AppRestarter/Core/Services/ICrashMonitorService.cs
@@ -0,0 +1,14 @@
+﻿using AppRestarter.Models;
+
+namespace AppRestarter.Core.Services
+{
+    public interface ICrashMonitorService
+    {
+        delegate void SignalMonitorTerminationEvent(WatchedApp app);
+        event SignalMonitorTerminationEvent OnSignalMonitorTermination;
+
+        void RegisterCrashedApp(WatchedApp app);
+        void UnregisterCrashedApp(WatchedApp app);
+        void SignalMonitorTermination(WatchedApp app);
+    }
+}
